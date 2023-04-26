8eb0fbc6ec9aceff8828cfa800ab72ece7a49197
Gmel2004 <128310386+Gmel2004@users.noreply.github.com>
2023-04-26 20:26:24 +0500

    Add files via upload

---
+++ OOPLab6/List.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int key;
+	Node* next;
+};
+
+class Iterator {
+private:
+	Node* current;
+	friend class List;
+public:
+	Iterator() {
+		current = NULL;
+	};
+	Iterator(Node* node) {
+		current = node;
+	};
+	void operator ++ () {
+		current = current->next;
+	}
+	Node* operator *() const {
+		return current;
+	}
+};
+
+class List {
+public:
+	List() {};
+	List(int);
+	~List();
+	List& operator = (List&);
+	int& operator[] (int);
+	int operator () ();
+	friend ostream& operator << (ostream&, List&);
+	friend istream& operator >> (istream&, List&);
+	Iterator first();
+	Iterator last();
+	void operator << (int);
+private:
+	Node* lastNd, * current, * head;
+	int n = 0;
+	Iterator beg, end;
+};

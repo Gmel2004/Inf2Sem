d1f3cce350e4656e5873f68504df9973becd2c4f
Gmel2004 <128310386+Gmel2004@users.noreply.github.com>
2023-05-11 23:22:25 +0500

    Add files via upload

--- OOPLab4/OOPLab4.cpp
+++ OOPLab4/OOPLab4.cpp
@@ -1,14 +1,12 @@
 ﻿#include "Person.h"
 #include "Student.h"
 
-void getObject(Student& object) {//функция принимающая объект
+void getObject(Person& object) {//функция принимающая объект
 	object.setName("Дима");
 	object.setAge(15);
-	object.setSubject("Физика");
-	object.setMark(3);
 }
 
-Student ReturnObject() {//функция возвращающая объект
+Person ReturnObject() {//функция возвращающая объект
 	Student s("Антон", 21, "Информатика", 5);
 	return s;
 }
@@ -22,10 +20,11 @@ int main() {
 	Student s1("Олег", 23, "inf", 4);//У Олега, как и у любого человека есть имя и возвраст, а как у ученика - предмет и оценка
 	Student s2;
 	getObject(s2);//установка значений через функцию
-	cout << s2.getName() << ' ' << s2.getAge() << ' ' << s2.getSubject() << ' ' << s2.getMark() << '\n';//проверка значений
-	Student s3 = ReturnObject();//создание объекта через функцию
-	cout << s3;//проверка значений
-	cin >> s3;//считываем новые значения для s3
+	cout << s2;//проверка значений
+	Person p2 = ReturnObject();//создание объекта через функцию
+	cout << p2;//проверка значений
+	Student s3;
+	cin >> s3;//считываем значений для s3
 	s2 = s3;//заменяем значения s2 соответствующими значениями s3
 	cout << s2;//проверяем значение
 	s2.BadMark();//проверяем оценку (выведет сообщение, если плохая)

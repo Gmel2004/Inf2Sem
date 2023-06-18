7738cd78c45c34cdb172de62b31c9e2b3c9ed9f6
Gmel2004 <128310386+Gmel2004@users.noreply.github.com>
2023-06-18 21:44:35 +0500

    Add files via upload

---
+++ 4lab/4lab.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int m[100];
+    int n = 0;
+    while (true) {
+        cout << "Введите номер команды:\n1. Добавить элемент в очередь\n2. Удалить элемент из очереди\n";
+        int a;
+        cin >> a;
+        if (a == 1) {
+            cout << "Введите элемент: ";
+            cin >> m[n];
+            n++;
+        }
+        else if (a == 2) {
+            if (n == 0) {
+                cout << "Очередь пуста, невозможно произвести удаление!\n";
+            }
+            else {
+                for (int i = 1; i < n; i++) {
+                    m[i - 1] = m[i];
+                }
+                n--;
+            }
+        }
+        else {
+            cout << "Команды под таким номером не существует!\n";
+        }
+        if (n == 0) {
+            cout << "Очередь пуста";
+        }
+        else {
+            cout << "Очередь: ";
+            for (int i = 0; i < n; i++) {
+                cout << m[i] << ' ';
+            }
+        }
+        cout << '\n';
+    }
+    return 0;
+}
---
+++ 6lab/dx.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+	system("chcp 1251>NULL");
+	string str;
+	cout << "Введите предложение (длина предложения не должна превышать 100 символов!):\n";
+	getline(cin, str);
+	const int begin_y = -2;
+	const int end_y = 2;
+	int width = str.length();
+	const int size = end_y - begin_y + 1;
+	const int scale_y = 1;// масшатб по оси Y
+	int m[(size - 1) * scale_y + 1][100] = {};
+	for (int x = 0; x < width; x++) {
+		double y = 2 * sin(1.0 / 2 * x) - begin_y;
+		int y1 = (size - round(y) - 1) * scale_y;
+		m[y1][x] = 1;
+	}
+	cout << '\n';
+	for (int y = 0; y <= (size - 1) * scale_y; y++) {
+		for (int x = 0; x < width; x++) {
+			cout << (m[y][x] ? str[x] : ' ');
+		}
+		cout << '\n';
+	}
+	return 0;
+}
